27b62bc0376de7b74edc342e5767286c4e581462
NicollasDileo06 <nicgato2013@gmail.com>
2020-08-03 18:56:29 -0300

    boletim terminado

---
+++ Desktop/GERAL/Senai/01_Boletim/DDL.sql
@@ -0,0 +1,13 @@
+/* 
+DDL Data Definition Language
+*/
+-- Criar o banco
+CREATE DATABASE boletim;
+
+--Apagamos uma base de dados
+--DROP DATABASE boletim;
+
+/*Criamos a tabela Aluno */
+CREATE TABLE Aluno (
+idAluno INT PRIMARY KEY
+);
